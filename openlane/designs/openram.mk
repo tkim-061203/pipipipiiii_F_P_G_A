d19962b400188c58d352b84b956f544961d2338b
Kim_local <nntkim.work@gmail.com>
2026-03-18 15:36:18 +0700

    chore: remove deleted file

--- openlane/designs/openram.mk
+++
@@ -1,9 +0,0 @@
-# OpenRAM compiler configuration
-
-# The path to the OpenRAM compiler script
-# The path to the OpenRAM compiler script
-# Phổ biến là sram_compiler.py hoặc sram.py
-OPENRAM_COMPILER ?= $(HOME)/OpenRAM/sram_compiler.py
-
-# Additional OpenRAM options
-OPENRAM_OPTS ?= -v --keeptemp
